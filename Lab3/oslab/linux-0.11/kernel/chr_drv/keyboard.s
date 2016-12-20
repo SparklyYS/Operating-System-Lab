170c8c29d1ed886e9de1e8e11aceac48395433b5
SparklyYS <SparklyYS@gmail.com>
2016-12-20 23:52:09 +0800

    fix Lab3 report

--- Lab3/oslab/linux-0.11/kernel/chr_drv/keyboard.s
+++ Lab3/oslab/linux-0.11/kernel/chr_drv/keyboard.s
@@ -1,5 +1,5 @@
 # 1 "keyboard.S"
-# 1 "/home/ys/Documents/oslab/linux-0.11/kernel/chr_drv//"
+# 1 "/home/ys/Documents/Operating-System-Lab/Lab3/oslab/linux-0.11/kernel/chr_drv//"
 # 1 "<built-in>"
 # 1 "<command line>"
 # 1 "keyboard.S"
